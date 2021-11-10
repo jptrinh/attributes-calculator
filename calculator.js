3954c8dfc50e71fa1b4c872a2a0b12c75a21e6b2
jptrinh <jpvh.trinh@gmail.com>
2021-11-10 21:54:57 +0100

    Verify if input number is empty

--- calculator.js
+++ calculator.js
@@ -17,7 +17,11 @@ $(document).ready(function () {
         });
 
         $.each($("input[type='number']"), function () {
-            sum += Number(($(this).val() * ($(this).attr('calculator-value'))));
+            if ($(this).attr('calculator-value') = undefined) {
+                sum += Number(($(this).val()));
+            } else {
+                sum += Number(($(this).val() * ($(this).attr('calculator-value'))));
+            }   
         });
 
         $.each($("select[calculator-element='select'"), function () {
